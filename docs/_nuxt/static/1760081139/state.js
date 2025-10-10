a7b8d5d0e9a98ea9e9b328524122d3050a773b07
TaiPham-Persol <tai.pham@x-factory.jp>
2025-10-10 15:28:02 +0700

    Update docs folder - 2025-10-10 15:28:02

--- docs/_nuxt/static/1760081139/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest_PCT_HP\u002F_nuxt\u002Fstatic\u002F1760081139",layout:"home",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest_PCT_HP\u002F",assetsPath:"\u002Ftest_PCT_HP\u002F_nuxt\u002F",cdnURL:a}}}}(null));
