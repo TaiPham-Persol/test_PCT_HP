f20f8691eabd7dc9395048fd22ad6e345da69d77
TaiPham-Persol <tai.pham@x-factory.jp>
2026-02-24 10:14:10 +0700

    Update docs folder - 2026-02-24 10:14:09

---
+++ docs/_nuxt/static/1771902797/awards-recognition/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1771902797",layout:"default",error:a,serverRendered:true,routePath:"\u002Fawards-recognition",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
